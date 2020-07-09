67a4f8eb83c5cce3fa11f50efca123e8704b30fb
Eugene White <eugenewhite1994@gmail.com>
2020-07-09 20:35:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   removeArray = ["Fender", "Les Paul", "Charvel", "Ibanez"]
-  removeArray.shift
+  removeArray.unshift("Ibanez")
 end
 
 def remove_element_from_start_of_array(array)
