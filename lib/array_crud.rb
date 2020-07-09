8eb3eae44f3c6c3a6aa37bfe5764e24af946e686
Eugene White <eugenewhite1994@gmail.com>
2020-07-09 20:34:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,10 +14,10 @@ end
 def add_element_to_start_of_array(array, element)
 endOfArray = ["Me", "Kaelyn"]
 endOfArray.push("Luna")
-  
 end
 
 def remove_element_from_end_of_array(array)
+  removeArray = ["Fender", "Les Paul", "Charvel", "Ibanez"]
   
 end
 
