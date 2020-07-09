71ff5b9ffdc85f45b7c89df7292bc986144e8677
Eugene White <eugenewhite1994@gmail.com>
2020-07-09 20:31:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,16 +1,19 @@
 def create_an_empty_array
-  
+  empty_array = Array.new
 end
 
 def create_an_array
-  
+  newArray = ["Guitar", "Skateboarding", "Gaming", "Music"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  addArray = ["Me"]
+  addArray << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
+endOfArray = ["Me", "Kaelyn"]
+endOfArray.push("Luna")
   
 end
 
