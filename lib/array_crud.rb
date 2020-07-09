7343db2c638c1b1121de149ea362bd359125008b
Eugene White <eugenewhite1994@gmail.com>
2020-07-09 20:43:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-endOfArray = ["Me", "Kaelyn"]
-endOfArray.unshift("Luna")
+endOfArray = ["Me", "Kaelyn", "Luna"]
+endOfArray.unshift
 end
 
 def remove_element_from_end_of_array(array)
