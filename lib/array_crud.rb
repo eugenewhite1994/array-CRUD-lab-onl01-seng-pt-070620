6b352f7a82b9ab97d639d00040623dcc1c84e534
Eugene White <eugenewhite1994@gmail.com>
2020-07-09 20:46:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array2 = ["Taco", "Pizza", "Pasta"]
+  array2 = ["Taco", "Pizza", "wow"]
   array2.pop
 end
 
